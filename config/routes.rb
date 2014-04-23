16827e70aa0a79eeb9011f50e199cf813cb37801
Benjamin Paul Dunphy & Paul Ogawa <student@epicodus.com>
2014-04-23 11:55:26 -0700

    Add Stripe files, work in progress (not yet functional)

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Donation::Application.routes.draw do
   devise_for :users
   resources :companies, only: [:index, :create, :new, :show]
   resources :users
+  resources :charges
   root to: 'companies#index'
 end
